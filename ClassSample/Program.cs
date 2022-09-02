00b3bd686c56ef5c26be58ba0ddb587178cffb48
Alec_Gracelli <alecgracelli@gmail.com>
2022-09-02 15:09:41 -0300

    Add project files.

---
+++ ClassSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassSample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno();
+
+            a1.Nome = "Pedro";
+            a1.Idade = 30;
+
+            Aluno a2 = new Aluno();
+            a2.Nome = "Lauro";
+            a2.Idade = 18;
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ForStatement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForStatement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int a = 10; a > 0; a = a - 3)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
